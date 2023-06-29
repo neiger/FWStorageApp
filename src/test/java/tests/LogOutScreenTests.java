f476f17c58af5b9039bdf1438163bc3819444c62
neiger <neiger.serrano.wks@gmail.com>
2023-06-29 12:00:37 -0600

    added logout feature and link to TL ID TC

---
+++ src/test/java/tests/LogOutScreenTests.java
@@ -0,0 +1,33 @@
+package tests;
+
+import general.MobileDriverManager;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import screens.StorageSampleLoggedScreen;
+import screens.StorageSampleLoginScreen;
+import screens.WebViewBrowserScreen;
+
+public class LogOutScreenTests extends MobileDriverManager {
+
+    private StorageSampleLoginScreen storageSampleLoginScreen;
+    private WebViewBrowserScreen webViewBrowserScreen;
+    private StorageSampleLoggedScreen storageSampleLoggedScreen;
+
+    @BeforeMethod
+    public void setAuthSampleLoginScreen() {
+        storageSampleLoginScreen = new StorageSampleLoginScreen(getDriver());
+        assertTrue(storageSampleLoginScreen.verifyLoads(), basicErrorMsg("Unable to load the screen"));
+        storageSampleLoggedScreen = storageSampleLoginScreen.signedUser();
+        assertTrue(storageSampleLoggedScreen.verifyLoads(), basicErrorMsg("The logged in user was not loaded"));
+        assertAll();
+    }
+
+    @Test
+    public void FW_101_verifyThatUserCanlogOutTheSession() {
+        storageSampleLoginScreen = storageSampleLoggedScreen.signOutUserSession();
+        System.out.println("The user session has been closed successfully");
+        // This validation will be performed once it is implemented by dev
+        //assertTrue(storageSampleLoggedScreen.verifyLoads(), basicErrorMsg("Unable to load login screen"));
+        //assertAll();
+    }
+}
